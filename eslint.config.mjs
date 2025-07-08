35e70c7a2e98af152757f1d7e232795e3d8e6a83
priyankaa02 <priyanka@corma.io>
2025-07-08 09:48:37 +0400

    allowed any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": [
+        "warn",
+        { "ts-ignore": false }
+      ]
+    },
+  },
 ];
 
 export default eslintConfig;
